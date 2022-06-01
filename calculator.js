66d32024bff80425e79075850d6b1850c4086045
t0mmylam <62630715+t0mmylam@users.noreply.github.com>
2022-06-01 16:58:56 -0400

    display works with number buttons

--- calculator.js
+++ calculator.js
@@ -26,3 +26,31 @@ function operate(o, a, b) {
             return divide(a, b);
     }
 }
+
+function print(cur, num) {
+    if (cur == 0 & num == 0) {
+        return 0;
+    }
+    let newString = ""+cur+""+num;
+    console.log(newString);
+    if (newString.length > 11) {
+        return cur;
+    }
+    return parseInt(newString);
+}
+
+let number = 0;
+const display = document.querySelector('.display');
+display.textContent = number.toString();
+
+const numButtons = document.querySelectorAll('.num');
+numButtons.forEach(num => num.addEventListener('click', () => {
+    number = print(number, num.id);
+    display.textContent = number;
+}))
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    number = 0;
+    display.textContent = number;
+})
