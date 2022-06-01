97f0ea0fc122c34f744c2e584ecd1c21c0f26c3b
t0mmylam <62630715+t0mmylam@users.noreply.github.com>
2022-06-01 16:14:06 -0400

    finished calculator layout

--- calculator.js
+++ calculator.js
@@ -14,3 +14,15 @@ function divide(a, b) {
     return a/b;
 }
 
+function operate(o, a, b) {
+    switch(o) {
+        case 1:
+            return add(a, b);
+        case 2:
+            return subtract(a, b);
+        case 3:
+            return multiply(a, b);
+        case 4:
+            return divide(a, b);
+    }
+}
