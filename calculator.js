b81dce1f2efccb1c5fa160fd4575c23c6ab90859
t0mmylam <62630715+t0mmylam@users.noreply.github.com>
2022-06-02 01:29:05 -0400

    IT WORKS

--- calculator.js
+++ calculator.js
@@ -11,19 +11,13 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a/b;
+    return a / b;
 }
 
 function operate(o, a, b) {
-    switch(o) {
-        case 1:
-            return add(a, b);
-        case 2:
-            return subtract(a, b);
-        case 3:
-            return multiply(a, b);
-        case 4:
-            return divide(a, b);
+    if (o == 1) {
+        console.log('Add: ' + a + " & " + b);
+        return a + b;
     }
 }
 
@@ -31,26 +25,83 @@ function print(cur, num) {
     if (cur == 0 & num == 0) {
         return 0;
     }
-    let newString = ""+cur+""+num;
-    console.log(newString);
-    if (newString.length > 11) {
+    let newNum = parseInt("" + cur + "" + num);
+    if (newNum.toString().length > 11) {
         return cur;
     }
-    return parseInt(newString);
+    return newNum;
 }
 
-let number = 0;
+let current = 0;
+let number1 = 0;
+let number2 = 0;
+let operator = 0;
 const display = document.querySelector('.display');
-display.textContent = number.toString();
+
+function editnumone() {
+    console.log(current);
+    number1 = print(number1, current)
+    display.textContent = number1;
+}
+
+function editnumtwo() {
+    console.log('x: ' + current);
+    number2 = print(number2, current)
+    display.textContent = number2;
+}
 
 const numButtons = document.querySelectorAll('.num');
-numButtons.forEach(num => num.addEventListener('click', () => {
-    number = print(number, num.id);
-    display.textContent = number;
-}))
+Array.from(numButtons).forEach(num => {
+    num.addEventListener('click', () => {
+        current = num.id;
+    });
+});
+Array.from(numButtons).forEach(num => {
+    num.addEventListener('click', editnumone);
+});
+
+
+function clearOthers() {
+    const operators = document.querySelectorAll('.o');
+    Array.from(operators).forEach(o => {
+        o.classList.remove('selected');
+    });
+}
+
 
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', () => {
-    number = 0;
-    display.textContent = number;
-})
+    number1 = 0;
+    number2 = 0;
+    operator = 0;
+    clearOthers();
+    display.textContent = number1;
+});
+
+const neg = document.querySelector('#plusminus');
+
+neg.addEventListener('click', () => {
+    display.textContent = display.textContent * -1;
+    number1 *= -1;
+});
+
+const operators = document.querySelectorAll('.o');
+operators.forEach(o => {
+    o.addEventListener('click', function () {
+        clearOthers();
+        operator = o.id;
+        console.log(operator);
+        o.classList.add('selected');
+        Array.from(numButtons).forEach(num => {
+            num.removeEventListener('click', editnumone);
+            num.addEventListener('click', editnumtwo);
+        });
+    });
+});
+
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', function () {
+    clearOthers();
+    number1 = operate(operator, number1, number2);
+    display.textContent = number1;
+});
